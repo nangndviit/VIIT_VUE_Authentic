bdedec88f573c7d715876ce3ee4e436ff5347871
unknown <nangnd@viit.com.vn>
2024-02-19 17:27:57 +0700

    update

--- src/components/Details/Product.vue
+++ src/components/Details/Product.vue
@@ -1,120 +1,192 @@
 <template>
-    
-    <div class="container">
-        <div class="thanhngang">
+  <div class="container">
+    <div class="thanhngang"></div>
+
+    <div class="product__details">
+      <div class="product__details__image">
+        <div class="product__details_img">
+          <img
+            src="https://cdn.authentic-shoes.com/wp-content/uploads/2023/04/34534534_42c66f7f6ba2417896b4f1aaefe7d28b.jpg"
+            alt=""
+          />
         </div>
 
-        <div class="product__details">
+        <i class="fa-regular fa-heart"></i>
+      </div>
 
-            <div class="product__details__image">
-
-                <div class="product__details_img">
-                    <img src="https://cdn.authentic-shoes.com/wp-content/uploads/2023/04/34534534_42c66f7f6ba2417896b4f1aaefe7d28b.jpg" alt="">
-                </div>
-
-                <i class="fa-regular fa-heart"></i>
-
-            </div>
-            
-            <div class="product__details__content">
-
-                <div class="product__details__bran">
-                    <a href="">Air Jordan 1 Low</a>
-                </div>
-                
-                <div class="product__details__name">
-                    <h1>Giày Nike Air Jordan 1 Low ‘Panda’ DC0774-101</h1>
-                </div>
-
-                <div class="product__details__price">
-                    <bdi>3,900,000 ₫ – 4,500,000 ₫</bdi>
-                </div>
-
-                <div class="product__details__panel">
-                    <span class="details__panel">
-                        Trả sau  
-                        <span class="details__panel__price">
-                            1.950.000 đ
-                        </span>
-                         x2 kỳ với
-                    </span>
-                    <div class="details__action">
-                        <img src="https://assets.fundiin.vn/merchant/logo_transparent.png" alt="">
-                        <div class="details__action__icons">
-                            <i class="fa-solid fa-question"></i>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="details__discount">
-
-                    <div class="details__discount__icon">
-                        <div class="details__discount__icons">
-                            <i class="fa-solid fa-percent"></i>
-                        </div>
-                    </div>
-
-                    <div class="details__discount__content">
-                        <span>
-                            Giảm giá đến
-                            <strong>50%</strong>
-                            Khi thanh toán qua Fundiin.
-
-                            <a href="#">xem thêm</a>
-                        </span>
-                    </div>
-
-                </div>
-
-                <div class="details__size__cart">
-
-                    <div class="details__size">
-                        <table class="detailas__size_item">
-                            <tbody>
-                                <tr>
-                                    <th class="label">
-                                        <label for="">Kích Thước</label>
-                                    </th>
-                                    <td class="value">
-                                        <ul class="value__label__size">
-                                            <li class="value__label__size__items">36</li>
-                                            <li class="value__label__size__items">36.5</li>
-                                            <li class="value__label__size__items">37.5</li>
-                                            <li class="value__label__size__items">38</li>
-                                            <li class="value__label__size__items">38.5</li>
-                                            <li class="value__label__size__items">39</li>
-                                            <li class="value__label__size__items">40</li>
-                                            <li class="value__label__size__items">40.5</li>
-                                            <li class="value__label__size__items">41</li>
-                                            <li class="value__label__size__items">42</li>
-                                            <li class="value__label__size__items">44</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div class="details__cart">
-
-                    </div>
-
-                </div>
-                
-            </div>
+      <div class="product__details__content">
+        <div class="product__details__bran">
+          <a href="">Air Jordan 1 Low</a>
         </div>
 
-        
+        <div class="product__details__name">
+          <h1>Giày Nike Air Jordan 1 Low ‘Panda’ DC0774-101</h1>
+        </div>
+
+        <div class="product__details__price">
+          <bdi>3,900,000 ₫ – 4,500,000 ₫</bdi>
+        </div>
+
+        <div class="product__details__panel">
+          <span class="details__panel">
+            Trả sau
+            <span class="details__panel__price"> 1.950.000 đ </span>
+            x2 kỳ với
+          </span>
+          <div class="details__action">
+            <img src="https://assets.fundiin.vn/merchant/logo_transparent.png" alt="" />
+            <div class="details__action__icons">
+              <i class="fa-solid fa-question"></i>
+            </div>
+          </div>
+        </div>
+
+        <div class="details__discount">
+          <div class="details__discount__icon">
+            <div class="details__discount__icons">
+              <i class="fa-solid fa-percent"></i>
+            </div>
+          </div>
+
+          <div class="details__discount__content">
+            <span>
+              Giảm giá đến
+              <strong>50%</strong>
+              Khi thanh toán qua Fundiin.
+
+              <a href="#">xem thêm</a>
+            </span>
+          </div>
+        </div>
+
+        <div class="details__size__cart">
+          <div class="details__size">
+            <table class="detailas__size_item">
+              <tbody>
+                <tr>
+                  <th class="label">
+                    <label for="">Kích Thước</label>
+                  </th>
+                  <td class="value">
+                    <ul class="value__label__size">
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >36</span
+                          >
+                        </div>
+                      </li>
+
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >36.5</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >37</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >37.5</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >38</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >38.5</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >39</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >41</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >42</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >42.5</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >43</span
+                          >
+                        </div>
+                      </li>
+                      <li class="value__label__size__items">
+                        <div class="variable-item-contents">
+                          <span class="variable-item-span variable-item-span-button"
+                            >44</span
+                          >
+                        </div>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div class="details__cart">
+            <div class="detail__cart__icons">
+              <i class="fa-solid fa-phone"></i>
+            </div>
+
+            <div class="detail__cart__">
+              <h3>Gọi ngay để có giá tốt</h3>
+              <p>
+                <strong
+                  >Hotline HN : 0984918486 - 0785499555 - 72 Tây Sơn <br />Đống Đa</strong
+                >
+              </p>
+              <p>
+                <strong>Hotline HCM : 0786665444 - 561 Nguyễn Đình Chiểu Q.3</strong>
+              </p>
+              <p>Tổng đài hoạt động từ 10h00 - 22h00 mỗi ngày</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
