6c99d8e12a9b5b2a6434feaae6f8c8e718d87f13
unknown <nangnd@viit.com.vn>
2024-02-20 17:11:13 +0700

    update 20/02

--- src/components/Details/Product.vue
+++ src/components/Details/Product.vue
@@ -2,18 +2,25 @@
   <div class="container">
     <div class="thanhngang"></div>
 
-    <div class="product__details">
-      <div class="product__details__image">
-        <div class="product__details_img">
-          <img
-            src="https://cdn.authentic-shoes.com/wp-content/uploads/2023/04/34534534_42c66f7f6ba2417896b4f1aaefe7d28b.jpg"
-            alt=""
-          />
+    <div class="product__main">
+      <div class="product__details">
+        <div class="product__details__image">
+          <div class="product__details_img">
+            <img
+              src="https://cdn.authentic-shoes.com/wp-content/uploads/2023/04/34534534_42c66f7f6ba2417896b4f1aaefe7d28b.jpg"
+            />
+            <i class="fa-regular fa-heart"></i>
+            <div class="btn_img">
+              <div class="btn_left">
+                <i class="fa-solid fa-angle-left"></i>
+              </div>
+              <div class="btn_right">
+                <i class="fa-solid fa-angle-right"></i>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <i class="fa-regular fa-heart"></i>
       </div>
-
       <div class="product__details__content">
         <div class="product__details__bran">
           <a href="">Air Jordan 1 Low</a>
@@ -59,7 +66,9 @@
           </div>
         </div>
 
+        <!-- kích thước và cart -->
         <div class="details__size__cart">
+          <!-- kích thước -->
           <div class="details__size">
             <table class="detailas__size_item">
               <tbody>
@@ -86,9 +95,9 @@
                       </li>
                       <li class="value__label__size__items">
                         <div class="variable-item-contents">
-                          <span class="variable-item-span variable-item-span-button"
-                            >37</span
-                          >
+                          <span class="variable-item-span variable-item-span-button">
+                            37
+                          </span>
                         </div>
                       </li>
                       <li class="value__label__size__items">
@@ -160,25 +169,153 @@
               </tbody>
             </table>
           </div>
-
+          <!-- cart -->
           <div class="details__cart">
             <div class="detail__cart__icons">
               <i class="fa-solid fa-phone"></i>
             </div>
 
-            <div class="detail__cart__">
+            <div class="detail__cart__information">
               <h3>Gọi ngay để có giá tốt</h3>
-              <p>
+              <p class="information_HN_HCM">
                 <strong
                   >Hotline HN : 0984918486 - 0785499555 - 72 Tây Sơn <br />Đống Đa</strong
                 >
               </p>
-              <p>
+              <p class="information_HN_HCM">
                 <strong>Hotline HCM : 0786665444 - 561 Nguyễn Đình Chiểu Q.3</strong>
               </p>
-              <p>Tổng đài hoạt động từ 10h00 - 22h00 mỗi ngày</p>
+              <p class="information_TD">Tổng đài hoạt động từ 10h00 - 22h00 mỗi ngày</p>
             </div>
           </div>
+          <!-- shopping cart và add và installment -->
+          <div class="button__shopping__add">
+            <div class="button__shopping">
+              <div class="shopping_icons">
+                <i class="fa-solid fa-cart-shopping"></i>
+              </div>
+              <div class="shopping_cmt">
+                <p>MUA NGAY</p>
+              </div>
+            </div>
+            <div class="button__add">
+              <div class="add_cart">
+                <p>THÊM VÀO GIỎ HÀNG</p>
+              </div>
+              <div class="add_installment">
+                <div class="add_installment_icons">
+                  <i class="fa-solid fa-snowman"></i>
+                </div>
+                <div class="add_installment_cmt">
+                  <p class="installment_cmt_1">MUA TRẢ GÓP 0%</p>
+                  <p class="installment_cmt_2">QUA CÔNG TY TÀI CHÍNH</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="product_ccordion">
+          <div class="ccordion_item">
+            <a href="" class="ccordion_itemss">
+              <button class="toggle">
+                <i class="fa-solid fa-chevron-down"></i>
+              </button>
+              <span class="toggle_name">Hệ Thống Cửa Hàng</span>
+            </a>
+          </div>
+
+          <div class="ccordion_item">
+            <a href="" class="ccordion_itemss">
+              <button class="toggle">
+                <i class="fa-solid fa-chevron-down"></i>
+              </button>
+              <span class="toggle_name">Cam Kết Khách Hàng</span>
+            </a>
+          </div>
+
+          <div class="ccordion_item">
+            <a href="" class="ccordion_itemss">
+              <button class="toggle">
+                <i class="fa-solid fa-chevron-down"></i>
+              </button>
+              <span class="toggle_name">Chính Sách Đổi Trả, Bảo Hành</span>
+            </a>
+          </div>
+
+          <div class="ccordion_item">
+            <a href="" class="ccordion_itemss">
+              <button class="toggle">
+                <i class="fa-solid fa-chevron-down"></i>
+              </button>
+              <span class="toggle_name">Chính Sách Vận Chuyển</span>
+            </a>
+          </div>
+
+          <div class="ccordion_item">
+            <a href="" class="ccordion_itemss">
+              <button class="toggle">
+                <i class="fa-solid fa-chevron-down"></i>
+              </button>
+              <span class="toggle_name">Phương Thức Thanh Toán</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="describe">
+      <div class="row">
+        <div class="col small-12 large-12">
+          <div class="col-inner">
+            <h3 class="describe__h3">
+              <span class="describe__h3__span">Mô Tả</span>
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="tab-description">
+        <p>Thương hiệu: Nike</p>
+        <p>Nhà thiết kế: Peter Moore</p>
+        <p>Công nghệ: Air</p>
+        <p>Màu sắc: White/ Wolf Grey/ Aluminum</p>
+        <p>Kiểu dáng: Giày bóng rổ</p>
+        <p>Fit size: true to size hoặc up 0.5 size</p>
+        <p>Mã sản phẩm: DC0774-105</p>
+        <div
+          class="youtube-embed-wrapper"
+          style="position: relative; padding-bottom: 56.25%; height: 0px"
+        ></div>
+        <p>####</p>
+        <h3>
+          Lý do gì khiến Nike Wmns Air Jordan 1 Low ‘White Wolf Grey’ trở nên đặc biệt?
+        </h3>
+        <div class="tab-describe">
+          <p>
+            <p class="tab_describe_p">Nike Air Jordan 1 Low</p> có thể là đôi giày “hot” ở thời điểm hiện tại khi được lăng xê rộng rãi bởi
+            rất nhiều người nổi tiếng, nhất là các rapper nổi tiếng như Travis Scott,
+            Drake,v.v. Chỉ vài tháng trước mùa hè, <p class="tab_describe_p">Jordan</p> đã tung ra một số màu sắc đẹp
+            nhất trong năm và nó không có dấu hiệu hạ nhiệt khi sold out liên tục. Và đặc
+            biệt Nike đã bắt tay với <p class="tab_describe_p">Dior</p> để tạo ra một trong những siêu phẩm đáng mong
+            chờ nhất trong năm 2020.
+          </p>
+          <p>
+            Đương nhiên, không phải ai cũng có thể chi trả cho một đôi AJ1 Dior, nên Nike
+            đã làm ra bản phối màu “White Wolf Grey” để thỏa mãn những người yêu giày trên
+            thế giới với phối màu tương tự bản collab với Dior. Vẫn là một đôi AJ1 với
+            những chi tiết quá quen thuộc như chất liệu da cao cấp, Nike Swoosh, hay hình
+            Jumpman trên lưỡi gà,v.v. Tuy chỉ sở hữu một phối màu trung tính và không có
+            quá nhiều điểm nhấn gì quá đặc biệt, nhưng nó vẫn khiến giới sneakerhead phát
+            điên khi cháy hàng liên tục ở mọi nơi nó được bán.
+          </p>
+
+          <p>
+            Chúng tôi khuyên bạn nên rock đôi giày này cùng quần joggers dày màu xám và một chiếc áo khoác denim có cổ cho
+             những ngày đông hay đơn giản là với những chiếc áo phông để thể hiện lên cá tính của mình. Hiện sản phẩm được 
+             phân phối trực tiếp tại <p class="tab_describe_p">Authentic Shoes</p>, hãy qua với chúng mình để có những trải nghiệm tốt nhất nhé!
+          </p>
+          
         </div>
       </div>
     </div>
